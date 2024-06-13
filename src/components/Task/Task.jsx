a209f047b22e3e0926af46448dc40d0d23c89bbd
Irae Veras <irae.online@gmail.com>
2024-06-13 18:15:49 -0300

    refatoração

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -12,50 +12,86 @@ import './Task.css';
 export const Task = () => {
 
     const columns = [
+        
         {
             name: "ID",
             selector: row => row.id,
-            sortable: true
+            sortable: true,
+            width: '70px',
+            text: "center"
         },
         {
             name: "TAREFA",
             selector: row => row.task,
-            sortable: true
+            width: '30%',
+            sortable: true,
         },
         {
             name: "DATA LIMITE",
             selector: row => row.date_conpletion,
+            width: '125px',
             sortable: true
         },
         {
             name: "DATA DE CONCLUSÃO",
             selector: row => row.create_date_task,
+            width: '180px',
             sortable: true
-        }
+        },
+        {
+            name: "CONCLUIR",
+            cell: () => (
+                <Button >
+                    <FiCheckSquare />
+                </Button>
+            ),
+            width: '95px',
+            
+        },
+        {
+            name: "EDITAR",
+            cell: () => (
+                <Button >
+                    <FiEdit />
+                </Button>
+            ),
+            width: '80px',
+            
+        },
+        {
+            name: "EXCLUIR",
+            cell: () => (
+                <Button >
+                    <FiXSquare />
+                </Button>
+            ),
+            width: '85px',
+            
+        },
     ];
 
-createTheme('solarized', {
-        
-        text: {
-            primary: '#000000',
-            secondary: '#2aa198',
-        },
-        background: {
-            default: 'transparent',
-        },
-        context: {
-            background: '#2aa198',
-            text: '#FFFFFF',
-        },
-        divider: {
-            default: '#ddd',
-        },
-        action: {
-            button: 'rgba(0,0,0,.54)',
-            hover: 'rgba(0,0,0,.08)',
-            disabled: 'rgba(0,0,0,.12)',
-        },
-    }, 'dark');
+    createTheme('solarized', {
+            
+            text: {
+                primary: '#000000',
+                secondary: '#2aa198',
+            },
+            background: {
+                default: 'transparent',
+            },
+            context: {
+                background: '#2aa198',
+                text: '#FFFFFF',
+            },
+            divider: {
+                default: '#ddd',
+            },
+            action: {
+                button: 'rgba(0,0,0,.54)',
+                hover: 'rgba(0,0,0,.08)',
+                disabled: 'rgba(0,0,0,.12)',
+            },
+        }, 'dark');
 
     const data = api.map(item => {
         return (
@@ -81,5 +117,6 @@ createTheme('solarized', {
         fixedHeader
         theme="solarized"
         />
+        
     )
 }
